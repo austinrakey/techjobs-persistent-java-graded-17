2062bec11e20beb75600ecb02ff05cc98139ec38
austinrakey <rakeysound@gmail.com>
2024-06-12 23:31:10 -0500

    task 1

--- queries.sql
+++ queries.sql
@@ -1,5 +1,9 @@
 --Part 1
-
+--FIELD     TYPE   NULL   KEY   DEFAULT   EXTRA
+--id        int    NO     PRI   NULL
+--employer varchar(255)   YES   NULL
+--name     varchar(255)   YES   NULL
+--skills   varchar(255)   YES   NULL
 --Part 2
 
 --Part 3
