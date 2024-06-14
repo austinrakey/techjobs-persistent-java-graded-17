027055b6d969bb7e55c005b172db86608ccc7e3e
austinrakey <rakeysound@gmail.com>
2024-06-14 00:21:22 -0500

    all tests but SQL passing

--- queries.sql
+++ queries.sql
@@ -4,7 +4,11 @@
 --employer varchar(255)   YES   NULL
 --name     varchar(255)   YES   NULL
 --skills   varchar(255)   YES   NULL
+
 --Part 2
+SELECT name
+FROM employer
+WHERE location = 'St. Louis City';
 
 --Part 3
 
