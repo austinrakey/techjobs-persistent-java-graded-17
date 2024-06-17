33e9109b9153a5546d0fb610a17e32aed7b083f5
austinrakey <rakeysound@gmail.com>
2024-06-17 12:23:11 -0500

    site working, FindAllbyID test failing

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -21,7 +21,7 @@ import java.util.Optional;
  * Created by LaunchCode
  */
 @Controller
-@RequestMapping("jobs")
+//@RequestMapping("")
 public class HomeController {
 
     @Autowired
@@ -33,28 +33,12 @@ public class HomeController {
     @Autowired
     private JobRepository jobRepository;
 
-//    @RequestMapping("/")
-//    public String index(Model model) {
-//
-//        model.addAttribute("title", "MyJobs");
-//
-//        return "index";
-//    }
-
-    @RequestMapping("")
+    @RequestMapping("/")
     public String index(Model model) {
         model.addAttribute("title", "My Jobs");
         model.addAttribute("jobs", jobRepository.findAll());
         return "index";
     }
-
-//    @GetMapping("add")
-//    public String displayAddJobForm(Model model) {
-//	model.addAttribute("title", "Add Job");
-//        model.addAttribute("employers", employerRepository.findAll());
-//        model.addAttribute(new Job());
-//        return "add";
-//    }
 
     @GetMapping("add")
     public String displayAddJobForm(Model model) {
@@ -64,22 +48,6 @@ public class HomeController {
         model.addAttribute("skills", skillRepository.findAll());
         return "add";
     }
-
-//    @PostMapping("add")
-//    public String processAddJobForm(@ModelAttribute @Valid Job newJob,
-//                                    Errors errors, Model model, @RequestParam int employerId) {
-//
-//        if (errors.hasErrors()) {
-//            model.addAttribute("title", "Add Job");
-//            model.addAttribute("employers", employerRepository.findAll());
-//            return "add";
-//        }
-//
-//        newJob.setEmployer(employerRepository.findById(employerId).orElse(null));
-//        // Save newJob to the database or perform other operations
-//
-//        return "redirect:";
-//    }
 
     @PostMapping("add")
     public String processAddJobForm(@ModelAttribute @Valid Job newJob,
@@ -101,53 +69,15 @@ public class HomeController {
         }
         Employer employer = employerOptional.get();
 
-        // Fetch skills from skillRepository based on skill IDs
         List<Skill> skillObjs = (List<Skill>) skillRepository.findAllById(skills);
 
-        // Set employer and skills on newJob
         newJob.setEmployer(employer);
         newJob.setSkills(skillObjs);
 
-        // Save newJob to the database
         jobRepository.save(newJob);
 
-        return "redirect:/jobs";
+        return "redirect:/";
     }
-
-
-//    @PostMapping("add")
-//    public String processAddJobForm(@ModelAttribute @Valid Job newJob,
-//                                    Errors errors, Model model, @RequestParam int employerId,
-//                                    @RequestParam List<Integer> skills) {
-//
-//        if (errors.hasErrors()) {
-//            model.addAttribute("title", "Add Job");
-//            model.addAttribute("employers", employerRepository.findAll());
-//            model.addAttribute("skills", skillRepository.findAll());
-//            return "add";
-//        }
-//
-//        Optional<Employer> result = employerRepository.findById(employerId);
-//        if (result.isPresent()) {
-//            Employer employer = result.get();
-//            newJob.setEmployer(employer);
-//        } else {
-//            model.addAttribute("title", "Add Job");
-//            return "add";
-//        }
-//
-//        List<Skill> skillObjs = (List<Skill>) skillRepository.findAllById(skills);
-//        newJob.setSkills(skillObjs);
-//
-//        jobRepository.save(newJob);
-//        return "redirect:";
-//    }
-
-//    @GetMapping("view/{jobId}")
-//    public String displayViewJob(Model model, @PathVariable int jobId) {
-//
-//            return "view";
-//    }
 
     @GetMapping("view/{jobId}")
     public String displayViewJob(Model model, @PathVariable int jobId) {
